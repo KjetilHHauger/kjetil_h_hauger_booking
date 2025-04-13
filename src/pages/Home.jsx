e1728e6b40705a6f2723b4db37648ce0b106127e
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-04-13 20:45:25 +0200

    chore: minor padding tweak

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <div className="w-full mx-auto">
       <HeroVideo />
-      <section className="mx-auto px-20">
+      <section className="mx-auto px-4 sm:px-10 md:px-20">
         <h1 className="text-2xl font-bold mb-6 mt-6">Available Venues</h1>
         <Filter
           filters={filters}
