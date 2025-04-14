ba68053452f8f096ceda921192c71355310dd82f
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-04-14 23:38:00 +0200

    CHORE: Rename BookingFrom to SearchFrom

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function BookingForm() {
+export default function SearchForm() {
   const [location, setLocation] = useState("");
   const [guests, setGuests] = useState(2);
   const navigate = useNavigate();
