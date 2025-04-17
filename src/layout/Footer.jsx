d809cabf633246ee4701e3e96ef8867134b7ef61
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-04-18 00:26:12 +0200

    FIX: social hover and title

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -24,10 +24,10 @@ export default function Footer() {
         </section>
         <section className="flex-1 flex flex-col py-10 gap-2">
           <h6 className="text-heading-6 font-bold underline">Social</h6>
-          <a href="https://hudd.no" aria-label="Hudd">
+          <a href="https://hudd.no" aria-label="Hudd" title="Hudd">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8"
+              className="w-8 h-8 hover:text-[#121b22]"
               viewBox="7 10 38 52"
               fill="currentColor"
             >
@@ -37,16 +37,24 @@ export default function Footer() {
               />
             </svg>
           </a>
-          <a href="https://bsky.app" aria-label="BlueSky">
-            <svg fill="none" viewBox="0 0 64 57" className="w-8 h-8 ml-1">
+          <a href="https://bsky.app" aria-label="BlueSky" title="BlueSky">
+            <svg
+              fill="none"
+              viewBox="0 0 64 57"
+              className="w-8 h-8 ml-1 hover:text-[#1185FE]"
+            >
               <path
                 fill="currentColor"
                 d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"
               ></path>
             </svg>
           </a>
-          <a href="https://www.tumblr.com" aria-label="Tumblr">
-            <TumblrLogo size={32} weight="fill" />
+          <a href="https://www.tumblr.com" aria-label="Tumblr" title="Tumblr">
+            <TumblrLogo
+              size={32}
+              weight="fill"
+              className="hover:text-[#2c4762]"
+            />
           </a>
         </section>
       </section>
