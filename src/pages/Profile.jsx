ab13d3507b0bb53a0efab86e33089e055bdeea21
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-04-26 11:38:02 +0200

    FIX: Profile image

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -113,14 +113,15 @@ export default function Profile() {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 flex flex-col md:flex-row gap-8">
+    <div className="max-w-4xl mx-auto p-6 flex flex-col md:flex-row gap-8 h-screen">
       {/* Profile info */}
       <div className="flex-shrink-0 w-full md:w-1/3 text-center md:text-left">
-        <div className="w-32 h-32 mx-auto md:mx-0 mb-4 border rounded overflow-hidden">
+        <div className="w-40 h-40 mx-auto md:mx-0 mb-4 border rounded-full overflow-hidden">
           <img
             src={user.avatar?.url || "/brokenImage.png"}
             alt={user.name}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover "
+            style={{ objectPosition: "50% 15%" }}
           />
         </div>
         <h2 className="text-heading-4 font-bold">{user.name}</h2>
