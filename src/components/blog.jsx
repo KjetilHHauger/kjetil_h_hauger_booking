ab3e982973a62d7dc1393f9e2b7a211a4f9193c6
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-06 21:22:56 +0200

    FIX: light card styling and testing

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-export default function Blog({ username, limit = 5 }) {
+export default function Blog({ username }) {
   const API = import.meta.env.VITE_API_URL;
 
   const [posts, setPosts] = useState([]);
@@ -29,34 +29,37 @@ export default function Blog({ username, limit = 5 }) {
   if (!posts.length) return <p>No posts yet.</p>;
 
   return (
-    <section className="space-y-6">
-      <h2 className="text-heading-5 font-bold">Latest posts by {username}</h2>
-      <ul className="space-y-4">
+    <section className="space-y-6 mt-6">
+      <ul className="space-y-4 gap-4 flex">
         {posts.map((p) => {
           const { id, title, media, created } = p;
           return (
-            <li key={id} className="border rounded p-4 hover:shadow">
-              <h3 className="text-heading-6 font-semibold mb-1">
-                <Link
-                  to={`/blog/${id}`}
-                  state={{ post: p }}
-                  className="hover:underline"
-                >
-                  {title}{" "}
-                  <span className="text-body-xs text-gray-500">
-                    {" "}
-                    published {new Date(created).toLocaleDateString()}
-                  </span>
-                </Link>
-              </h3>
-
+            <li
+              key={id}
+              className="bg-amber-500 h-90 w-90 flex flex-col relative"
+            >
               {media?.url && (
                 <img
                   src={media.url}
                   alt={media.alt || title}
-                  className="max-w-2xl rounded mb-4"
+                  className="rounded object-fill"
                 />
               )}
+              <section className="mt-4 w-90 absolute bottom-0 left-0 text-white bg-black/20">
+                <h3 className="text-heading-6 font-semibold h-13 ml-5">
+                  <Link
+                    to={`/blog/${id}`}
+                    state={{ post: p }}
+                    className="hover:underline"
+                  >
+                    {title}{" "}
+                  </Link>
+                </h3>
+                <span className="text-body-xs ml-5">
+                  {" "}
+                  published {new Date(created).toLocaleDateString()}
+                </span>
+              </section>
             </li>
           );
         })}
