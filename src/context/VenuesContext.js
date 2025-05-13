2d397b96656a73d8578f3ea5a6df9c6d228284c5
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-13 19:56:53 +0200

    FEAT: Load on website load

---
+++ src/context/VenuesContext.js
@@ -0,0 +1,2 @@
+import { createContext } from "react";
+export default createContext({ venues: [], loading: true, error: null });
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,22 +1,27 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useVenues } from "../hooks/useVenues";
+import VenuesContext from "../context/VenuesContext";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 export default function Layout() {
+  const venuesData = useVenues();
+
   return (
     <div className="text-font-body flex flex-col min-h-screen max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-8">
-      <Nav />
-      <main className="flex-grow mb-10 ">
-        <Outlet />
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          theme="colored"
-        />
-      </main>
-      <Footer />
+      <VenuesContext.Provider value={venuesData}>
+        <Nav />
+        <main className="flex-grow">
+          <Outlet />
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            theme="colored"
+          />
+        </main>
+        <Footer />
+      </VenuesContext.Provider>
     </div>
   );
 }
