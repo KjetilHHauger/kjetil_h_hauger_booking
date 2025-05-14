7f853e5d04e51affb1b49e6cb7ea807f0f82fc45
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-14 23:28:28 +0200

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-7xl mx-auto text-brand-primary px-8 sm:px-10 md:px-20 gap-10 my-10">
       <Link to={"/"} className="text-3xl font-bold mb-4">
-        <img src="/public/assets/404.webp" alt="404 page not found" />
+        <img src="/assets/404.webp" alt="404 page not found" />
       </Link>
     </div>
   );
