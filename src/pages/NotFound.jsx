82b6aee52d286fd9109c34d3be9207f6690b7a91
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-20 18:56:21 +0200

    FIX: minor margin tweak

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-7xl text-center mx-auto text-brand-primary px-8 sm:px-10 md:px-20 gap-10 my-10">
       <Link to={"/"} className="text-3xl font-bold mb-4">
-        <div className="flex items-center justify-center gap-4 mt-44">
+        <div className="flex items-center justify-center gap-4 md:mt-28">
           <svg
             version="1.0"
             xmlns="http://www.w3.org/2000/svg"
